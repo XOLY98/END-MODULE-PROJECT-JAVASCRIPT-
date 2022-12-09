25110de5488d8d88fa944240fe880b4a87c0b6e8
XOLELWA MANQINA <manqinaxolelwa98@gmail.com>
2022-12-09 17:01:30 +0200

    code

--- code.js
+++ code.js
@@ -113,7 +113,7 @@ let hairProducts=[
               <p class="card-text">${item.productColor}<br>${item.productPrice}<br>${item.productSize}
               </p>
               <p>
-              <a href="admin.html" class="btn btn-primary" >Go somewhere</a>
+              <a href="../MY HTML CODES/admin.html" class="btn btn-danger" >Go somewhere</a>
               </p>
             </div>
           </div>`
@@ -230,14 +230,77 @@ let adminList=[
 
     async function show() {
         let info =document.querySelector('#prods');
-        adminList.forEach((item)=> {
-        info.innerHTML +=`<tr><td class="col">${item.productName}</td>
-                          <td>${item.productColor}</td>
-                          <td>${item.productPrice}</td>
-                          <td>${item.productSize}</td>
-                          <td><button type ='button'></button></td>
-                          <td><button type='button></td>`
-                          
+        info.innerHTML = '';
+        adminList.forEach((item,index)=> {
+        info.innerHTML +=`
+        <tr>
+        <td>${item.productName}</td>
+        <td>${item.productColor}</td>
+        <td>${item.productPrice}</td>
+        <td>${item.productSize}</td>
+        <td><button type="button" class="btn btn-danger"id="bp" data-bs-toggle="modal" data-bs-target="#hair-products-edit" onclick="editProducts(${index})">Edit</button></td>
+        <td><button id="Xolelwa" type="clear" class="btn btn-danger" onclick="deleteProduct">Delete</button></td>
+        </tr>`
+                         
+        console.log(index)
         })
     }
     show();
+
+//ADD BUTTON//
+
+      
+    let addProduct=()=>{
+        let id = parseInt(document.querySelector(`#id`).value);
+        let productName = document.querySelector(`#type`).value;
+        let productPrice = document.querySelector(`#price`).value;
+        let productColor = document.querySelector(`#color`).value;
+        let productSize = document.querySelector(`#size`).value;
+        let image = document.querySelector(`#image`).value;
+        adminList.push({
+            id,
+            productName,
+            productPrice,
+            productColor,
+            productSize,
+            image,
+        })
+        console.log(adminList)
+        localStorage.setItem("Xole", JSON.stringify(adminList));
+        show();
+
+    }
+    document.getElementById("hair-products") .addEventListener("click", addProduct);
+
+        //Edit BUTTONS//
+ function editProducts(i) {
+    // go to array 
+    // filter array by the id
+    // push to temp array
+    // diplay new data
+
+    console.log(i)
+    console.log(adminList[i])
+    let arr = adminList[i]
+        console.log(arr.adminList)
+        document.querySelector(`#editid`).value = arr.id
+        document.querySelector(`#edittype`).value = arr.productName
+        document.querySelector(`#editprice`).value = arr.productPrice
+         document.querySelector(`#editcolor`).value=arr.productColor
+        document.querySelector(`#editsize`).value=arr.productSize;
+        document.querySelector(`#editimage`).value=arr.image;
+        array[id-1].name=id;
+        array[id-1].name=productName ;
+        array[id-1].name=productPrice;
+        array[id-1].name=productColor;
+        array[id-1].name=productSize;
+        array[id-1].name=image;
+ }
+document.getElementById("#bp").addEventListener("onclick", (editProducts));
+
+
+//DELETE BUTTON//
+
+
+ 
+
